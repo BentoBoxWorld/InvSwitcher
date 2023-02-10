236995a818c312fd686d891b8c90efbf05c1b812
tastybento <tastybento@wasteofplastic.com>
2023-02-09 21:31:46 -0800

    Added PlayerListener test class

--- src/test/java/com/wasteofplastic/invswitcher/dataObjects/InventoryStorageTest.java
+++ src/test/java/com/wasteofplastic/invswitcher/dataObjects/InventoryStorageTest.java
@@ -1,6 +1,9 @@
 package com.wasteofplastic.invswitcher.dataObjects;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
 import java.util.List;
@@ -13,7 +16,6 @@ import org.bukkit.Material;
 import org.bukkit.Statistic;
 import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemStack;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
