791694801acd446824c4679953ae806db800a400
tastybento <tastybento@wasteofplastic.com>
2023-02-09 22:23:46 -0800

    Clean up test class

--- src/test/java/com/wasteofplastic/invswitcher/InvSwitcherTest.java
+++ src/test/java/com/wasteofplastic/invswitcher/InvSwitcherTest.java
@@ -9,7 +9,6 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -25,20 +24,12 @@ import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Set;
-import java.util.UUID;
 import java.util.jar.JarEntry;
 import java.util.jar.JarOutputStream;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Server;
-import org.bukkit.UnsafeValues;
 import org.bukkit.World;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemFactory;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.plugin.PluginManager;
-import org.bukkit.scheduler.BukkitScheduler;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -46,8 +37,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.stubbing.Answer;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -59,41 +48,22 @@ import world.bentobox.bentobox.BentoBox;
 import world.bentobox.bentobox.Settings;
 import world.bentobox.bentobox.api.addons.Addon.State;
 import world.bentobox.bentobox.api.addons.AddonDescription;
-import world.bentobox.bentobox.api.addons.GameModeAddon;
-import world.bentobox.bentobox.api.user.User;
 import world.bentobox.bentobox.database.DatabaseSetup.DatabaseType;
-import world.bentobox.bentobox.database.objects.Island;
 import world.bentobox.bentobox.managers.AddonsManager;
-import world.bentobox.bentobox.managers.FlagsManager;
-import world.bentobox.bentobox.managers.IslandWorldManager;
-import world.bentobox.bentobox.managers.IslandsManager;
-import world.bentobox.bentobox.managers.PlaceholdersManager;
 
 /**
  * @author tastybento
  *
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Bukkit.class, BentoBox.class, User.class})
+@PrepareForTest({Bukkit.class, BentoBox.class})
 public class InvSwitcherTest {
 
     private static File jFile;
     @Mock
-    private User user;
-    @Mock
-    private IslandsManager im;
-    @Mock
-    private Island island;
-    @Mock
     private BentoBox plugin;
     @Mock
-    private FlagsManager fm;
-    @Mock
-    private GameModeAddon gameMode;
-    @Mock
     private AddonsManager am;
-    @Mock
-    private BukkitScheduler scheduler;
 
     @Mock
     private Settings pluginSettings;
@@ -103,13 +73,7 @@ public class InvSwitcherTest {
     @Mock
     private Logger logger;
     @Mock
-    private PlaceholdersManager phm;
-    @Mock
     private World world;
-    private UUID uuid;
-
-    @Mock
-    private PluginManager pim;
 
     @BeforeClass
     public static void beforeClass() throws IOException {
@@ -147,35 +111,6 @@ public class InvSwitcherTest {
         when(plugin.getSettings()).thenReturn(pluginSettings);
         when(pluginSettings.getDatabaseType()).thenReturn(value);
 
-        // Player
-        Player p = mock(Player.class);
-        // Sometimes use Mockito.withSettings().verboseLogging()
-        when(user.isOp()).thenReturn(false);
-        uuid = UUID.randomUUID();
-        when(user.getUniqueId()).thenReturn(uuid);
-        when(user.getPlayer()).thenReturn(p);
-        when(user.getName()).thenReturn("tastybento");
-        User.setPlugin(plugin);
-
-        // Island World Manager
-        IslandWorldManager iwm = mock(IslandWorldManager.class);
-        when(plugin.getIWM()).thenReturn(iwm);
-
-        // Player has island to begin with
-        when(im.getIsland(Mockito.any(), Mockito.any(UUID.class))).thenReturn(island);
-        when(plugin.getIslands()).thenReturn(im);
-
-        // Locales
-        // Return the reference (USE THIS IN THE FUTURE)
-        when(user.getTranslation(Mockito.anyString())).thenAnswer((Answer<String>) invocation -> invocation.getArgument(0, String.class));
-
-        // Server
-        PowerMockito.mockStatic(Bukkit.class);
-        Server server = mock(Server.class);
-        when(Bukkit.getServer()).thenReturn(server);
-        when(Bukkit.getLogger()).thenReturn(Logger.getAnonymousLogger());
-        when(Bukkit.getPluginManager()).thenReturn(mock(PluginManager.class));
-
         // Addon
         addon = new InvSwitcher();
         File dataFolder = new File("addons/Level");
@@ -185,39 +120,13 @@ public class InvSwitcherTest {
         addon.setDescription(desc);
         // Addons manager
         when(plugin.getAddonsManager()).thenReturn(am);
-        // One game mode
-        when(am.getGameModeAddons()).thenReturn(Collections.singletonList(gameMode));
-        AddonDescription desc2 = new AddonDescription.Builder("bentobox", "BSkyBlock", "1.3").description("test").authors("tasty").build();
-        when(gameMode.getDescription()).thenReturn(desc2);
-        when(gameMode.getOverWorld()).thenReturn(world);
-
-        // Flags manager
-        when(plugin.getFlagsManager()).thenReturn(fm);
-        when(fm.getFlags()).thenReturn(Collections.emptyList());
-
 
         // Bukkit
         PowerMockito.mockStatic(Bukkit.class);
-        when(Bukkit.getScheduler()).thenReturn(scheduler);
-        ItemMeta meta = mock(ItemMeta.class);
-        ItemFactory itemFactory = mock(ItemFactory.class);
-        when(itemFactory.getItemMeta(any())).thenReturn(meta);
-        when(Bukkit.getItemFactory()).thenReturn(itemFactory);
-        UnsafeValues unsafe = mock(UnsafeValues.class);
-        when(unsafe.getDataVersion()).thenReturn(777);
-        when(Bukkit.getUnsafe()).thenReturn(unsafe);
-        when(Bukkit.getPluginManager()).thenReturn(pim);
-
         when(Bukkit.getWorld(anyString())).thenReturn(world);
-
-        // placeholders
-        when(plugin.getPlaceholdersManager()).thenReturn(phm);
 
         // World
         when(world.getName()).thenReturn("bskyblock-world");
-        // Island
-        when(island.getWorld()).thenReturn(world);
-        when(island.getOwner()).thenReturn(uuid);
     }
 
     /**
@@ -269,7 +178,6 @@ public class InvSwitcherTest {
     @Test
     public void testOnLoad() {
         addon.onLoad();
-        verify(plugin).log("No config.yml found. Creating it...");
         File file = new File("config.yml");
         assertTrue(file.exists());
     }
